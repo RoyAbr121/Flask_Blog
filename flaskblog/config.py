abd17fd6ef4c8292ba95428fe44ae8412c6e3cb3
royabr121 <royabr121@gmail.com>
2023-07-30 16:03:55 +0300

    With Blueprings implemented

---
+++ flaskblog/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+class Config:
+    MAIL_SERVER = 'smtp.google.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
